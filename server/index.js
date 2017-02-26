202d7a75e496ff06dc054a0fb129cbe63a1347eb
Avnerus <avnerus@gmail.com>
2017-02-26 12:40:02 +0200

    Server

--- server/index.js
+++ server/index.js
@@ -1,12 +1,19 @@
 import express from 'express';
-import socketio from 'socket.io'
+//import socketio from 'socket.io'
 import _ from 'lodash';
+
+import Labels from '../labels/labels'
 
 const app = express();
 const server = require('http').Server(app);
-const io = socketio(server);
+//const io = socketio(server);
 
 app.use(express.static('public'));
+
+app.get('/label', function (req, res) {
+  // Choose a random label
+  res.send(_.sample(Labels.labels));
+})
 
 server.listen(3000, function () {
       console.log('listening on port 3000!');
